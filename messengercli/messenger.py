929eef27458ca802e5a44099763530d91a9097de
linsy king <linsy_king@sjtu.edu.cn>
2023-05-04 14:14:51 +0800

    fix: docs

--- messengercli/messenger.py
+++ messengercli/messenger.py
@@ -237,27 +237,14 @@ class Messenger:
 @app.command()
 def init(
     name: str,
-    use_full: bool = typer.Option(
-        True,
-        prompt="""Thanks for using Messenger.
-There are currently two main versions you can use.
-The core version only has the basic framework. There are no core engine support. Use this version if you want to create your own game engine.
-The full version includes a core engine template which has a basic 2D physics engine and high-performance game component support. It enables you to create a level quickly.
-See more instructions on https://github.com/linsyking/Messenger.git
-Do you want to install the full version of Messerger?""",
-    ),
 ):
-    if use_full:
-        version = "full"
-        print("The full version is not supported yet...")
-        exit(0)
-    else:
-        version = "core"
     input(
-        f"""Here is my plan:
+        f"""Thanks for using Messenger.
+See https://github.com/linsyking/Messenger.git for more information.
+Here is my plan:
 - Create a directory named {name}
-- Use the {version} Messenger version
-- Elm install the packages needed
+- Install the core Messenger liberary
+- Install the elm packages needed
 Press Enter to continue
 """
     )
